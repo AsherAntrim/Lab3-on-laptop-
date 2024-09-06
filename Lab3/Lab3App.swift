78c4cf490ba3858ff83b6a73809bd9db10cc8daa
Asher <aantrim246@cedarville.edu>
2024-09-06 14:06:03 -0400

    Initial Commit

---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by Asher Antrim on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

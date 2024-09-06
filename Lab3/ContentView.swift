a629bb7ee0284dcb7ce36ee5e158db161d39b64f
Asher <aantrim246@cedarville.edu>
2024-09-06 14:47:55 -0400

    everything done but the changing of the emoji changing when tapped. (I am close though I think lol)

--- Lab3/ContentView.swift
+++ Lab3/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+
+private struct Resturant: Identifiable {
+    var name: String
+    var id: String { name }
+}
+private let restaurants: [Resturant] = [
+    Resturant(name: "McDonalds's"),
+    Resturant(name: "Arby's"),
+    Resturant(name: "Wendy's"),
+    Resturant(name: "Pizza Hut"),
+    Resturant(name: "Culver's")]
+    
+struct ResturantView: View {
+    var body: some View {
+        ForEach(restaurants) { Resturant in
+            ZStack {
+                RoundedRectanglesStandard()
+                HStack {
+                    Text(Resturant.name)
+                        .font(.title)
+                        .foregroundColor(.white)
+                    Spacer()
+                    EmojiTap()
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ResturantView()
         }
         .padding()
+    }
+}
+
+struct RoundedRectanglesStandard: View {
+    var body: some View {
+        RoundedRectangle
+            .init(cornerRadius: 25)
+            .fill(Color.blue)
+    }
+}
+
+struct EmojiTap: View {
+    @State private var emoji: Text = 👎
+    
+    var body: some View {
+        Text(emoji)
+            .onTapGesture(count: 1) {
+                emoji = 👍
+            }
     }
 }
 
